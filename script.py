5630cdc41c9c055c1d0dd2e697f7051c22b6dc49
yvonnendutaw <yvonita7@gmail.com>
2016-04-27 10:15:56 +0300

    call and response

--- script.py
+++ script.py
@@ -11,3 +11,14 @@ def tip(bill):
 meal_cost = 100
 meal_with_tax = tax(meal_cost)
 meal_with_tip = tip(meal_cost)
+
+#calls and responds at square(10)
+def square(n):
+    """Returns the square of a number."""
+    squared = n**2
+    print "%d squared is %d." % (n, squared)
+    return squared
+
+# Call the square function on line 9! Make sure to
+# include the number 10 between the parentheses.
+square(10)
