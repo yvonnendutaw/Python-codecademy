904f9da9c7cc33e71fd8cccfa50d7dfd3d847d65
yvonnendutaw <yvonita7@gmail.com>
2016-04-29 00:31:54 +0300

    deleting the keys and values

--- dictionaries.py
+++ dictionaries.py
@@ -18,3 +18,23 @@ menu['beef'] = 72
 
 print "There are " + str(len(menu)) + " items on the menu."
 print menu
+
+#deleting the keys and their values
+# key - animal_name : value - location
+zoo_animals = { 'Unicorn' : 'Cotton Candy House',
+'Sloth' : 'Rainforest Exhibit',
+'Bengal Tiger' : 'Jungle House',
+'Atlantic Puffin' : 'Arctic Exhibit',
+'Rockhopper Penguin' : 'Arctic Exhibit'}
+# A dictionary (or list) declaration may break across multiple lines
+
+# Removing the 'Unicorn' entry. (Unicorns are incredibly expensive.)
+del zoo_animals['Unicorn']
+
+# Your code here!
+del zoo_animals['Sloth']
+del zoo_animals['Bengal Tiger']
+zoo_animals['Rockhopper Penguin'] = ['Gen']
+
+
+print zoo_animals
