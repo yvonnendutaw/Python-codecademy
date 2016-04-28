324ec9cd0195759a6fe125b99d7be623580f652e
yvonnendutaw <yvonita7@gmail.com>
2016-04-29 00:22:01 +0300

    adding new items to dictionary

--- dictionaries.py
+++ dictionaries.py
@@ -5,3 +5,16 @@ print residents['Puffin'] # Prints Puffin's room number
 
 print residents['Sloth']
 print residents['Burmese Python']
+
+#Adding items to the disctionaries
+menu = {} # Empty dictionary
+menu['Chicken Alfredo'] = 14.50 # Adding new key-value pair
+print menu['Chicken Alfredo']
+
+# Your code here: Add some dish-price pairs to menu!
+menu['fries'] = 100
+menu['fish'] = 89
+menu['beef'] = 72
+
+print "There are " + str(len(menu)) + " items on the menu."
+print menu
