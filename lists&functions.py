dd554f3b7d731fe7a48a7cacd49d501f9b984f47
yvonnendutaw <yvonita7@gmail.com>
2016-05-02 20:46:14 +0300

    range and iteration over a list in a function

--- lists&functions.py
+++ lists&functions.py
@@ -8,3 +8,21 @@ def double_list(x):
 # Don't forget to return your new list!
 
 print double_list(n)
+
+
+#returns range
+def my_function(x):
+    for i in range(0, len(x)):
+        x[i] = x[i] * 2
+    return x
+
+print my_function(range(3)) # Add your range between the parentheses!
+
+
+#iterating over a list in a function
+n = [3, 5, 7]
+def total(numbers):
+    result = 0
+    for i in range(len(numbers)):
+        result += numbers[i]
+    return result
