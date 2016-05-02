c3fa1ccbf6ec49b9ab698123867f7cad13aa289f
yvonnendutaw <yvonita7@gmail.com>
2016-05-02 20:58:35 +0300

    using strings in functions and joining two lists

--- lists&functions.py
+++ lists&functions.py
@@ -26,3 +26,23 @@ def total(numbers):
     for i in range(len(numbers)):
         result += numbers[i]
     return result
+
+#using strings in lists in functions
+n = ["Michael", "Lieberman"]
+# Add your function here
+def join_strings(words):
+    result = ""
+    for i in range(len(words)):
+        result += words[i]
+    return result
+print join_strings(n)
+
+#joining lists
+m = [1, 2, 3]
+n = [4, 5, 6]
+
+def join_lists(x,y):
+    return x + y
+
+print join_lists(m, n)
+# You want this to print [1, 2, 3, 4, 5, 6]
