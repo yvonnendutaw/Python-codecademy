96455a04e0ea522a29c210dbc0af35872e119f31
yvonnendutaw <yvonita7@gmail.com>
2016-05-02 09:13:52 +0300

    getting avarage

--- dictionaries.py
+++ dictionaries.py
@@ -191,3 +191,103 @@ def compute_bill(food):
             total = total
     print total
     return total
+
+#for the record
+students = [lloyd, alice, tyler]
+lloyd = {
+    "name": "Lloyd",
+    "homework": [90.0, 97.0, 75.0, 92.0],
+    "quizzes": [88.0, 40.0, 94.0],
+    "tests": [75.0, 90.0]
+}
+alice = {
+    "name": "Alice",
+    "homework": [100.0, 92.0, 98.0, 100.0],
+    "quizzes": [82.0, 83.0, 91.0],
+    "tests": [89.0, 97.0]
+}
+tyler = {
+    "name": "Tyler",
+    "homework": [0.0, 87.0, 75.0, 22.0],
+    "quizzes": [0.0, 75.0, 78.0],
+    "tests": [100.0, 100.0]
+}
+
+print lloyd["name"]
+print lloyd["homework"]
+print lloyd["quizzes"]
+print lloyd["tests"]
+
+print alice["name"]
+print alice["homework"]
+print alice["quizzes"]
+print alice["tests"]
+
+print tyler["name"]
+print tyler["homework"]
+print tyler["quizzes"]
+print tyler["tests"]
+
+
+#getting the average
+lloyd = {
+    "name": "Lloyd",
+    "homework": [90.0, 97.0, 75.0, 92.0],
+    "quizzes": [88.0, 40.0, 94.0],
+    "tests": [75.0, 90.0]
+}
+alice = {
+    "name": "Alice",
+    "homework": [100.0, 92.0, 98.0, 100.0],
+    "quizzes": [82.0, 83.0, 91.0],
+    "tests": [89.0, 97.0]
+}
+tyler = {
+    "name": "Tyler",
+    "homework": [0.0, 87.0, 75.0, 22.0],
+    "quizzes": [0.0, 75.0, 78.0],
+    "tests": [100.0, 100.0]
+}
+
+# Add your function below!
+def average(numbers):
+    total = sum(numbers)
+    total = float(total) / float(len(numbers))
+    return total
+
+
+
+ #just weight and see
+
+def get_average(student):
+    homework = average(student["homework"])* 0.1
+    quizzes = average(student["quizzes"])* 0.3
+    tests = average(student["tests"])* 0.6
+
+    avrg = homework + quizzes + tests
+    return avrg
+
+#getting letter
+def get_letter_grade(score):
+    if score>= 90:
+        return 'A'
+    elif score>= 80:
+        return 'B'
+    elif score>= 70:
+        return 'C'
+    elif score >= 60:
+        return 'D'
+    else:
+        return 'F'
+
+print get_letter_grade(get_average(lloyd))
+print get_letter_grade(get_average(alice))
+print get_letter_grade(get_average(tyler))
+
+students=[lloyd, alice, tyler]
+
+def get_class_average(students):
+    results=[]
+    for student in students:
+        results.append(get_average(student))
+    return average(results)
