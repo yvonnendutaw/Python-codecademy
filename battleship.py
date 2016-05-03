c1027c85e27a040415f39cfd7edca4844ca99df8
yvonnendutaw <yvonita7@gmail.com>
2016-05-03 04:43:59 +0300

    printing pretty, hiding elements

--- battleship.py
+++ battleship.py
@@ -16,3 +16,37 @@ def print_board(board):
     for row in board:
         print row
 
+
+
+#printing pretty
+board = []
+for i in (range(0,5)):
+    result = ["O"] * 5
+    board.append(result)
+def print_board(board):
+    for row in board:
+        print " ".join(row)
+
+#hiding some elements from the user
+from random import randint
+
+board = []
+
+for x in range(0, 5):
+    board.append(["O"] * 5)
+
+def print_board(board):
+    for row in board:
+        print " ".join(row)
+board
+# Add your code below!
+def random_row(board):
+    return randint(0, len(board) - 1)
+def random_col(board):
+    return randint(0, len(board) - 1)
+
+print random_row(board)
+print random_col(board)
+
+
+
