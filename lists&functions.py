156cb05f05c22fa920351eb9bf3e59d0832a3c7b
yvonnendutaw <yvonita7@gmail.com>
2016-05-03 04:11:09 +0300

    using two lists in a function

--- lists&functions.py
+++ lists&functions.py
@@ -46,3 +46,17 @@ def join_lists(x,y):
 
 print join_lists(m, n)
 # You want this to print [1, 2, 3, 4, 5, 6]
+
+
+
+#using two lists in  a function
+
+n = [[1, 2, 3], [4, 5, 6, 7, 8, 9]]
+# Add your function here
+def flatten(lists):
+    results = []
+    for i in range(len(lists)):
+        for j in range(len(lists[i])):
+            results.append(lists[i][j])
+    return results
+print flatten(n)
