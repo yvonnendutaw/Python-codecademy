86215b3f60f4ecdca141e6ccb6afac399ac4b657
yvonnendutaw <yvonita7@gmail.com>
2016-05-03 04:25:22 +0300

    battleship.py

---
+++ battleship.py
@@ -0,0 +1,18 @@
+#creating a grid size of Os
+board = []
+for i in (range(0,5)):
+    result = ["O"] * 5
+    board.append(result)
+    print board
+
+
+
+#printing row by row of the board lists
+board = []
+for i in (range(0,5)):
+    result = ["O"] * 5
+    board.append(result)
+def print_board(board):
+    for row in board:
+        print row
+
