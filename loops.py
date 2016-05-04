c2c3eb3fdb8c70259a70ab7e70a11fb402dd4559
yvonnendutaw <yvonita7@gmail.com>
2016-05-04 04:54:49 +0300

    loops.py basics

---
+++ loops.py
@@ -0,0 +1,23 @@
+#while counts to 9
+count = 0
+
+if count < 5:
+    print "Hello, I am an if statement and count is", count
+
+while count < 10:
+    print "Hello, I am a while and count is", count
+    count +=
+
+    #how the loop checks for condition
+    loop_condition = True
+
+while loop_condition:
+    print "I am a loop"
+    loop_condition = False
+
+    #using while with arithmetic operations
+    num = 1
+
+while num <= 10:  # Fill in the condition
+    print num ** 2
+    num += 1
