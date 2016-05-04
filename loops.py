0f3a608995adacf87d2dfaf8286fc4aad82e410b
yvonnendutaw <yvonita7@gmail.com>
2016-05-04 11:30:08 +0300

    prints from index 1 and not zero

--- loops.py
+++ loops.py
@@ -100,3 +100,32 @@ for key in d:
     # Your code here!
  print key, d[key]
 
+
+##prints index from 1 intead of zero
+choices = ['pizza', 'pasta', 'salad', 'nachos']
+
+print 'Your choices are:'
+for index, item in enumerate(choices):
+    print index+1, item
+
+##printing lists max number
+list_a = [3, 9, 17, 15, 19]
+list_b = [2, 4, 8, 10, 30, 40, 50, 60, 70, 80, 90]
+
+for a, b in zip(list_a, list_b):
+    # Add your code here!
+    if a < b:
+        print b
+    else:
+        print a
+
+##for statement with else
+fruits = ['banana', 'apple', 'orange', 'tomato', 'pear', 'grape']
+
+print 'You have...'
+for f in fruits:
+    if f == 'tomato':
+        print 'A tomato is not a fruit!' # (It actually is.)
+    print 'A', f
+else:
+    print 'A fine selection of fruits!'
